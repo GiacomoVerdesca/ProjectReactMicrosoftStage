80956d94ae12de7d3546bdaa0782e9e8b1065f1f
Gian20luca <gianluca.bellafronte@gmail.com>
2021-02-01 15:17:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 
 import React from 'react';
-import { AuthComponent } from './components/authComponent/AuthComponent';
-import { NavbarComponent } from './core/navbarComponent/NavbarComponent';
 function App() {
   return (
     <div>
-      <NavbarComponent />
     </div>
   );
 }
--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-export const config = {
-  appId: "765221b8-fb88-4f07-a1ef-b40f3ad08d91",
-  redirectURI: "http://localhost:3000/",
-  scopes: ["user.read",'OnlineMeetings.ReadWrite'],
-};
